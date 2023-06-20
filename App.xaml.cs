446ff90ffe366a74430e3b5bf1ae7a1300f7bacc
Jistx <116097008+Jistx@users.noreply.github.com>
2023-06-20 11:26:26 -0400

    update file

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace SAM
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
